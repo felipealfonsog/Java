f67e0aed35e617c0fa8dcc42ef09659e3d1e7d07
Felipe <f.alfonso@res-ear.ch>
2018-07-19 23:16:18 -0400

    changes

---
+++ Nested.java
@@ -0,0 +1,10 @@
+// iterations || counting with  nested loops
+
+private void example1() {
+    int n = 10; 
+    for (int j = 1; <= n ; j ++) {
+        for (int k = 1; k <= j ; k++) {
+            System.out.print("*");
+        }
+    }
+}
