a16f31a917970e153ec819b00f61649b9d066c87
Felipe <f.alfonso@res-ear.ch>
2018-08-27 02:42:03 -0300

    updates..

--- test.interv.js
+++ test.interv.js
@@ -2,13 +2,38 @@
 
 
 - Conceptos POO
+
+Conceptos Básicos de Programación Orientada a Objetos
+http://codejavu.blogspot.com/2013/05/conceptos-de-programacion-orientada.html
+
+
+
 - "First class citizen functions" en Javascript
+https://en.wikipedia.org/wiki/First-class_function
+
 - ¿Que es NodeJS?
+https://devcode.la/blog/que-es-nodejs/
+
 - Non-blocking I/O de NodeJS
+
+https://stackoverflow.com/questions/10570246/what-is-non-blocking-or-asynchronous-i-o-in-node-js
+
+
 - ¿Que es ExpressJS?
+framework de desarrollo para nodejs
+
 - Callback Hell y alternativas de como solucionarlo 
+
+https://codearmy.co/que-es-el-callback-hell-y-como-evitarlo-4af418a6ed14
+
+
 - Herramientas para Test Unitarios en Javascript
+
+https://es.wikipedia.org/wiki/Prueba_unitaria
+
 - ¿Que es Gulp?
+es una libreria que permite crear cambios a medida que se desarrolla algo en nodejs
+
 - GIT (conceptos de commit, merge, pull, push, pull o merge request)
 - ¿Que servicios de Amazon AWS conoce y/o usado?
 
